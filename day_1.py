8957324b9b10b440b422a56f812a031430e8bb40
katelocate <katelocat3@gmail.com>
2022-11-11 21:19:26 +0300

    Add the 3rd exercize to the day_1.py.

--- day_1.py
+++ day_1.py
@@ -1,6 +1,18 @@
-# Fix the code below 👇
+# Exercise 2: Fix the code below
+
+# print(Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+#   print('e.g. print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n.")
 
 print("Day 1 - String Manipulation")
 print("String Concatenation is done with the \"+\" sign.")
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
+
+
+# Exercise 3: Write a program that prints the number of characters in a user's name.
+# You might need to Google for a function that calculates the length of a string.
+
+name = input('What is your name?')
+print(len(name))
