53579f9ec40287381afd593df3e5b9462d45d1af
katelocate <katelocat3@gmail.com>
2022-11-12 19:03:43 +0300

    Fix the band name output.

--- day_1/project_of_the_day.py
+++ day_1/project_of_the_day.py
@@ -4,7 +4,7 @@ GREETING = 'Welcome to the Band Name Generator.'
 ASK_CITY = 'What`s the name of the city you grew up in?'
 ASK_PET_NAME = 'What`s your pet`s name?'
 NAME_COMPONENTS = (ASK_CITY, ASK_PET_NAME)
-TELL_BAND_NAME = 'Your band name could be'
+TELL_BAND_NAME = 'Your band name could be {}'
 SPACE = ' '
 
 print(GREETING)
@@ -15,4 +15,4 @@ for component in NAME_COMPONENTS:
     band_name += SPACE
 band_name.rstrip(SPACE)
 
-print(TELL_BAND_NAME, band_name)
+print(TELL_BAND_NAME.format(band_name))
