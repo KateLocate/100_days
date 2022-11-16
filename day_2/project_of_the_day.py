75e4be0a4228778b41a85701b010c47fdcac1605
katelocate <katelocat3@gmail.com>
2022-11-16 22:16:37 +0300

    Fix: rewrite get_number function using check option that takes lambda functions as a value.

--- day_2/project_of_the_day.py
+++ day_2/project_of_the_day.py
@@ -1,4 +1,6 @@
 # https://replit.com/@appbrewery/tip-calculator-end
+from typing import Callable
+
 
 class TipCalculator:
     GREETING = 'Welcome to the tip calculator!'
@@ -8,36 +10,32 @@ class TipCalculator:
     NUMBER_OF_PEOPLE = (1, float('inf'))
     ASK_NUMBER_OF_PEOPLE = 'How many people to split the bill?'
     TELL_THE_RESULT = 'Each person should pay: ${:.2f}'
-    VALUE_ERROR_MESSAGE = 'What you entered is not a number.'
+    BILL_VALUE_ERROR_MESSAGE = 'Please, enter positive number.'
+    PEOPLE_VALUE_ERROR_MESSAGE = 'Please, use positive integers greater than 0, e.g. 1, 2, 5.'
     TIP_ERROR_MESSAGE = 'Please, choose the tip amount from the given options.'
 
-    def get_number(
-            self, data_type: type, input_message: str, error_message: str,
-            values: tuple = None, interval: tuple = None
-    ):
+    def get_number(self, data_type: type, input_message: str, error_message: str, check: Callable = None):
         try:
             result = data_type(input(input_message))
         except ValueError:
             print(error_message)
             result = self.get_number(data_type, input_message, error_message)
-        if values:
-            if result not in values:
+        if check:
+            if not check(result):
                 print(error_message)
-                result = self.get_number(data_type, input_message, error_message, values=values)
-        if interval:
-            start, stop = interval
-            if not (start <= result <= stop):
-                print(error_message)
-                result = self.get_number(data_type, input_message, error_message, interval=interval)
+                result = self.get_number(data_type, input_message, error_message, check=check)
         return result
 
     def run_calculator(self):
         print(self.GREETING)
 
-        bill = self.get_number(float, self.ASK_TOTAL_BILL, self.VALUE_ERROR_MESSAGE)
-        tip_percent = self.get_number(int, self.ASK_TIP_PERCENT, self.TIP_ERROR_MESSAGE, values=self.TIP_PERCENTS)
+        bill = self.get_number(float, self.ASK_TOTAL_BILL, self.BILL_VALUE_ERROR_MESSAGE, check=lambda x: x >= 0)
+        tip_percent = self.get_number(
+            int, self.ASK_TIP_PERCENT, self.TIP_ERROR_MESSAGE, check=lambda x: x in self.TIP_PERCENTS
+        )
         num_of_people = self.get_number(
-            int, self.ASK_NUMBER_OF_PEOPLE, self.VALUE_ERROR_MESSAGE, interval=self.NUMBER_OF_PEOPLE
+            int, self.ASK_NUMBER_OF_PEOPLE, self.PEOPLE_VALUE_ERROR_MESSAGE,
+            check=lambda x: self.NUMBER_OF_PEOPLE[0] < x < self.NUMBER_OF_PEOPLE[1]
         )
 
         bill_with_tip = bill + tip_percent * bill / 100
