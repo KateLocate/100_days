34fa0f81eded15de088d4a58f4433f3cfd133aba
katelocate <katelocat3@gmail.com>
2022-11-18 23:05:42 +0300

    Add the marks, connecting the scenario.

--- day_3/story_game.py
+++ day_3/story_game.py
@@ -4,46 +4,55 @@
 # 'You stopped in a small town, that you stumbled upon while travelling. You meet a man, who swears to you
 #  that he can tell you treasure whereabouts. He asks you to pay 3 coins for this information.'
 # 'Will you pay?'
-# 'Yes, I will!' 'No, I won`t...'
+# 'Yes, I will!' 0
+# 'No, I won`t...' 1
 
 # 0
 # 'The man takes you to the road that will lead you to a tree with the hidden treasure and shows you the direction.
 #  Then he leaves.'
 # 'You are walking down the road and see the fork at your path. You can see a lake shore in the distance to the left
 #  and thick woods to the right. Which way to go: left or right?'
+# 'I choose left!' 0.0
+# 'I choose right!' 0.1
 
 # 1
-# 'You leave the town and continue your journey, but not this quest!'
+# 'You leave the town and continue your journey, but not this quest!' 1.0
 
 # 1.0
 # 'Game over!'
+# 1.1
+# 'The end'
 
 # 0.0
 # 'You walk the path to the lake shore and enjoy the weather and fresh water.'
 # 'You can see a boat, sailing in your direction.
 # An old man asks you to do him a small favour and he`ll give you a ride in return.'
-# 'Do you agree?' 'Yes, I will help.' 'I`ll go my own way.'
+# 'Do you agree?'
+# 'Yes, I will help.' 0.0.0
+# 'I`ll go my own way.' 0.0.1
 
 # 0.0.0
 # 'You get on the boat and the man asks you to help him draw two nets from the water.
 # You manage doing this, and you sail along the shore.'
 # 'As the boat begins to shake and the water fills it from the inside, you start drowning.
 # You now have a choice: save an old man or grab your belongings and try to rescue. What will you choose?'
-# 'No time for doubts, I`ll save him!' 'I cannot just leave all my possessions this way...'
+# 'No time for doubts, I`ll save him!' 0.0.0.1
+# 'I cannot just leave all my possessions this way...' 0.0.0.0
 
 # 0.1
 # 'You start making the way through the forest, following the narrow trail.
 #  Some time later you get stuck and suddenly see a dog, friendly barking and trying to get your attention.
 #  You feel like it wants to show you the way, will you follow?.'
-# 'Yes, it seems friendly, why not.'
-# 'It is a distraction, I won`t go.'
+# 'Yes, it seems friendly, why not.' 0.1.0
+# 'It is a distraction, I won`t go.' 0.1.1
 
 # 0.0.0.0
-# 'You both drown, and it is the end of your story...'
+# 'You both drown, and it is the end of your story...' 1.0
 
 # 0.0.0.1
 # 'You both survive! You saved a man and got to the shore all wet and tired. The old man is grateful
 #  and tells you where to go, to find the grove where the tree might be. You find the grove easily and enter it.'
+#  0.0.1.0
 
 # 0.0.1
 # 'Walking along the shore for quite a long time, you find a road leading to a grove,
@@ -52,37 +61,41 @@
 # 0.0.1.0
 # 'Not far from here you can hear the gentle music and quiet laughter.'
 # 'Will you go and find out what is happening there?'
-# 'Sounds great! Maybe they`ll guide me?' 'No, I should not be distracted.'
+# 'Maybe they`ll guide me? I`ll check.' 0.0.1.0.1
+# 'No, I should not be distracted.' 0.0.1.0.0
 
 # 0.0.1.0.0
 # 'You go on searching and finally see golden light, enveloping the crown of the tree in the distance.
 # You come closer and see the hollow in it, where the treasure lies.
 # There are a lot of coins there and a ring with a blue stone, that might be magical!
-# Congratulations, you got your reward and finished this quest! See ya, adventurer!'
+# Congratulations, you got your reward and finished this quest! See ya, adventurer!' 1.1
 
 # 0.1.0
-# 'You follow the dog and it leads you to the grove, where you might find the tree!'
+# 'You follow the dog and it leads you to the grove, where you might find the tree!' 0.0.1.0
 
 # 0.1.1
 # 'You ignore the dog and go on searching the way.
 # There is an opening in the trees wall and violet light pouring through, will you go that way?'
-# 'No, this may be dangerous, what if it is a trap?'
-# 'Yes, I don`t think it is suspicious.'
+# 'No, this may be dangerous, what if it is a trap?' 0.1.1.1
+# 'Yes, I don`t think it is suspicious.' 0.1.1.0
 
 # 0.1.1.1
-# 'You go deeper and deeper into the forest. You got lost and never came back, nobody knows what has happened to you.'
+# 'You go deeper and deeper into the forest.
+# You got lost and never came back, nobody knows what has happened to you.' 1.0
 
 # 0.1.1.0
-# 'You come closer, violet light goes off, and you see the grove, where the tree might be.'
+# 'You come closer, violet light goes off, and you see the grove, where the tree might be.' 0.0.1.0
 
 # 0.0.1.1
 # 'When you come to a source of music and laughter, you see the enchanted harp,
 # playing songs and sounding different voices. There is a chest nearby, will you open it?'
+# 'Maybe there is loot? I`ll open it.' 0.0.1.1.0
+# 'No, I won`t.' 0.0.1.1.1
 
 # 0.0.1.1.0
 # 'You try opening the chest, but suddenly something tong-like comes out of it and grabs you.
-# You are the mimic`s prey. Sorry, the quest ends here...'
+# You are the mimic`s prey. Sorry, the quest ends here...' 1.0
 
 # 0.0.1.1.1
 # 'You sit in front of a harp and enjoy the enveloping song, it clouds your mind and feels so good.
-# You have nothing to worry about anymore, you go back to the town and forget about the tree treasure.'
+# You have nothing to worry about anymore, you go back to the town and forget about the tree treasure.' 1.0
