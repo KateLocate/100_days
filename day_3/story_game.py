b5a22aa7f15e6af8eec5c8517db75d23626880f1
katelocate <katelocat3@gmail.com>
2022-11-18 21:51:59 +0300

    Add a part of the scenario text.

---
+++ day_3/story_game.py
@@ -0,0 +1,42 @@
+# 'Welcome to the Treasure Hunter Quest!'
+# 'Only the bravest will overcome all the obstacles! Good luck, adventurer!'
+
+# 'You stopped in a small town, that you stumbled upon while travelling. You meet a man, who swears to you
+#  that he can tell you treasure whereabouts. He asks you to pay 3 coins for this information.'
+# 'Will you pay?'
+# 'Yes, I will!' 'No, I won`t...'
+
+# 0
+# 'The man takes you to the road that will lead you to a tree with the hidden treasure and shows you the direction.
+#  Then he leaves.'
+# 'You are walking down the road and see the fork at your path. You can see a lake shore in the distance to the left
+#  and thick woods to the right. Which way to go: left or right?'
+
+# 1
+# 'You leave the town and continue your journey, but not this quest!'
+
+# 1.0
+# 'Game over!'
+
+# 0.0
+# 'You walk the path to the lake shore and enjoy the weather and fresh water.'
+# 'You can see a boat, sailing in your direction.
+# An old man asks you to do him a small favour and he`ll give you a ride in return.'
+# 'Do you agree?' 'Yes, I will help.' 'I`ll go my own way.'
+
+# 0.0.0
+# 'You get on the boat and the man asks you to help him draw two nets from the water.
+# You manage doing this, and you sail along the shore.'
+# 'As the boat begins to shake and the water fills it from the inside, you start drowning.
+# You now have a choice: save an old man or grab your belongings and try to rescue. What will you choose?'
+# 'No time for doubts, I`ll save him!' 'I cannot just leave all my possessions this way...'
+
+# 0.1
+# 'You start making the way through the forest, following the narrow trail.'
+
+# 0.0.0.0
+# 'You both drown, and it is the end of your story...'
+
+# 0.0.0.1
+# 'You both survive! You saved a man and got to the shore all wet and tired. The old man is grateful
+#  and tells you where to go, to find the grove where the tree might be. You find the grove easily and enter it.'
