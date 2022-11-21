3253ab6a701b072936ad8a7a1139747c41bc99ce
katelocate <katelocat3@gmail.com>
2022-11-21 15:35:50 +0300

    Fix: organize printing using get_fixed_length_string and text_with_borders_printing methods.

--- day_3/story_game.py
+++ day_3/story_game.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 import ascii_art as art
 from scenario import GREETING, SCENARIO
 
@@ -43,38 +45,68 @@ class Story:
             answer = input(phrase)
         return answer
 
+    @staticmethod
+    def get_fixed_length_string(string: str, max_length: int) -> str:
+        new_string = ''
+        if len(string) > max_length:
+            space = ' '
+            list_from_string = string.split(space)
+            current_max_length = max_length
+            for word in list_from_string:
+                string_part = word + space
+                if len(new_string) + len(string_part) < current_max_length:
+                    new_string += string_part
+                else:
+                    new_string += '\n' + string_part
+                    current_max_length += max_length
+            new_string.rstrip(space)
+        return new_string if new_string else string
+
+    def text_with_borders_printing(self, top_border: str, bottom_border: str,
+                                   text: str = '', action: Callable = None, action_args: tuple = ()):
+        result = None
+        brd_length = len(top_border.split('\n')[0])
+        print(top_border)
+        if text:
+            text_processed = self.get_fixed_length_string(text, max_length=brd_length)
+            print(text_processed)
+        if action:
+            result = action(*action_args)
+        print(bottom_border)
+        return result
+
     def play(self):
         start_event = self.story.get('')
         current_event = start_event
+
         while current_event.left_event or current_event.right_event:
-            print(art.TOP_BORDER)
-            print(current_event.text)
-            print(art.BOTTOM_BORDER)
-            left = current_event.left
-            right = current_event.right
-            if right and left:
-                print(f'1:{current_event.left} \n2:{current_event.right}')
-                choice_phrase = 'Enter 1 or 2:'
-                print(art.TOP_BORDER)
-                choice = self.infinite_query(choice_phrase, ['1', '2'])
-                print(art.BOTTOM_BORDER)
-                if choice == '1':
-                    current_event = current_event.left_event
-                elif choice == '2':
-                    current_event = current_event.right_event
-            elif current_event.right_event and current_event.left_event:
+            self.text_with_borders_printing(art.TOP_BORDER, art.BOTTOM_BORDER, text=current_event.text)
+
+            left, left_text, left_event = '1', current_event.left, current_event.left_event
+            right, right_text, right_event = '2', current_event.right, current_event.right_event
+
+            if right_text and left_text:
+                print(f'{left}:{current_event.left} \n{right}:{current_event.right}')
+                choice_phrase = f'Enter {left} or {right}:'
+                choices = [left, right]
+                choice = self.text_with_borders_printing(
+                    art.TOP_BORDER, art.BOTTOM_BORDER, action=self.infinite_query, action_args=(choice_phrase, choices)
+                )
+                if choice == left:
+                    current_event = left_event
+                elif choice == right:
+                    current_event = right_event
+            elif right_event and left_event:
                 print('See some bugs here, some fat ones...')
-            elif current_event.right_event or current_event.left_event:
-                current_event = current_event.right_event or current_event.left_event
+            elif right_event or left_event:
+                current_event = right_event or left_event
         else:
             print(current_event.text)
 
     def start_game(self):
         print(art.START_ART)
-        print(art.START_AND_STOP)
-        print(self.greeting)
+        self.text_with_borders_printing(art.START_AND_STOP, art.START_AND_STOP, self.greeting)
         self.play()
-        print(art.START_AND_STOP)
 
 
 story = Story(SCENARIO, GREETING)
